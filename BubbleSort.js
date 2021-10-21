239ff02e54722743c8790ff6d47bdf31917ff7ae
ins-arun <60340001+ins-arun@users.noreply.github.com>
2021-10-22 00:56:29 +0530

    Update BubbleSort.js

--- BubbleSort.js
+++ BubbleSort.js
@@ -3,21 +3,29 @@
  * 
  * Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
  */
-function bubbleSort(arr) {
-    let noSwaps = true
-    for (let index1 = arr.length; index1 > 0; index1--) {
-        for (let index2 = 0; index2 < index1 - 1; index2++) {
-            if (arr[index2] > arr[index2 + 1]) {
-                let temp = arr[index2 + 1]
-                arr[index2 + 1] = arr[index2]
-                arr[index2] = temp
-                noSwaps = false
-            }
-        }
-        if (noSwaps) break // if no swap is there then break (way to optimize)
-    }
-    return arr
-}
+function bblSort(arr){
+     
+ for(var i = 0; i < arr.length; i++){
+     
 
-// input your elements here
-console.log(bubbleSort([2, 6, 7, 2, 3, 4, 10, 16, 11, 14]))
+   for(var j = 0; j < ( arr.length - i -1 ); j++){
+       
+ 
+     if(arr[j] > arr[j+1]){
+         
+     
+       var temp = arr[j]
+       arr[j] = arr[j + 1]
+       arr[j+1] = temp
+     }
+   }
+ }
+ 
+ console.log(arr);
+}
+  
+
+var arr = [234, 43, 55, 63,  5, 6, 235, 547];
+  
+
+bblSort(arr);
